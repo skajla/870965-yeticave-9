5648b0f7eaee0d04632deb303627f3f76f015320
Виктория <viktoriya_2306@mail.ru>
2019-05-05 15:24:17 +0300

    Шаблонизация проекта2

--- index.php
+++ index.php
@@ -4,32 +4,16 @@ require_once('helpers.php');
 $is_auth = rand(0, 1);
 $user_name = 'Виктория';
 
-function esc($str) {
-    $text = htmlspecialchars($str);
-
-    return $text;
-}
-
 $categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
 
-for ($i = 0; $i < sizeof($categories); $i++) {
-    $categories[$i] = esc($categories[$i]);
-}
-
 $announcements = [
-    ['title' => '<h1>2014 Rossignol District Snowboard<h1>', 'category' => 'Доски и лыжи', 'cost' => 10999, 'img_path' => 'img/lot-1.jpg'],
+    ['title' => '2014 Rossignol District Snowboard', 'category' => 'Доски и лыжи', 'cost' => 10999, 'img_path' => 'img/lot-1.jpg'],
     ['title' => 'DC Ply Mens 2016/2017 Snowboard', 'category' => 'Доски и лыжи', 'cost' => 159999, 'img_path' => 'img/lot-2.jpg'],
     ['title' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'category' => 'Крепления', 'cost' => 8000, 'img_path' => 'img/lot-3.jpg'],
     ['title' => 'Ботинки для сноуборда DC Mutiny Charocal', 'category' => 'Ботинки', 'cost' => 10999, 'img_path' => 'img/lot-4.jpg'],
     ['title' => 'Куртка для сноуборда DC Mutiny Charocal', 'category' => 'Одежда', 'cost' => 7500, 'img_path' => 'img/lot-5.jpg'],
     ['title' => 'Маска Oakley Canopy', 'category' => 'Разное', 'cost' => 5400, 'img_path' => 'img/lot-6.jpg']
 ];
-
-for ($i = 0; $i < sizeof($announcements); $i++) {
-    foreach ($announcements[$i] as $key => $value) {
-        ($announcements[$i])[$key] = esc($value);
-    }
-}
 
 function format_cost ($cost) {
     $cost = ceil($cost);
@@ -49,7 +33,7 @@ $content_arguments = [
 
 $content = include_template('index.php', $content_arguments);
 
-$layoutConfig = [
+$layout_config = [
     'title' => 'Главная',
     'is_auth' => $is_auth,
     'user_name' => $user_name,
@@ -57,7 +41,7 @@ $layoutConfig = [
     'categories' => $categories
 ];
 
-$layout = include_template('layout.php', $layoutConfig);
+$layout = include_template('layout.php', $layout_config);
 
 print($layout);
 ?>
--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,7 @@
         <ul class="promo__list">
             <?php foreach ($categories as $value) { ?>
                 <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $value ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($value) ?></a>
             </li>
             <?php } ?>
         </ul>
@@ -17,11 +17,11 @@
             <?php foreach ($announcements as $announcement) { ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?= $announcement['img_path'] ?>" width="350" height="260" alt="">
+                        <img src="<?= htmlspecialchars($announcement['img_path']) ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?= $announcement['category'] ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $announcement['title'] ?></a></h3>
+                        <span class="lot__category"><?= htmlspecialchars($announcement['category']) ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($announcement['title']) ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
